919c697f6c9deb0d3caff9abe75533f36ac6aba0
badr <bernardoantoniod@gmail.com>
2023-10-24 19:09:28 -0300

    find - findIndex - includes

---
+++ arrayManipulation/server/09-find.js
@@ -0,0 +1,29 @@
+const numbers = [1, 30, 49, 29, 10, 13];
+
+const orders = [
+    {
+        customerName: "Nicolas",
+        total: 60,
+        delivered: true,
+    },
+    {
+        customerName: "Zulema",
+        total: 120,
+        delivered: false,
+    },
+    {
+        customerName: "Santiago",
+        total: 180,
+        delivered: false,
+    },
+    {
+        customerName: "Bernardo",
+        total: 1000,
+        delivered: true,
+    }
+];
+
+console.log(numbers.findIndex(num => num === 30))
+
+const special = orders.findIndex(order => order.total === 180)
+console.log(special)
---
+++ arrayManipulation/server/10-includes.js
@@ -0,0 +1,4 @@
+const pets = ["cat", "dog", "bat"];
+
+isADog = pets.includes("cat")
+console.log(isADog)
