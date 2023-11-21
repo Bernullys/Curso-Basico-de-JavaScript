b7136f5f892312f441edd05fbbbd42d5db1f4f15
badr <bernardoantoniod@gmail.com>
2023-11-21 20:19:23 -0300

    recursive with python

--- ejemplos/sumOdDigits.py
+++ ejemplos/sumOdDigits.py
@@ -1,5 +1,16 @@
 num1 = 493193
 
-num1_to_str = str(num1)
+def digital_root(n):
+    num1_to_str = str(n)
+    num1_as_array = []
+    for character in num1_to_str:
+        num1_as_array.append(int(character))
+    sum = 0
+    for s in num1_as_array:
+        sum += s
+    if sum > 9:
+        return digital_root(sum)
+    return sum
 
-print (num1_to_str.split())
+
+print(digital_root(num1))
--- ejemplos/sumOfDigits.js
+++ ejemplos/sumOfDigits.js
@@ -4,6 +4,7 @@ const number2 = 942
 
 const digitalRoot = (n) => {
     const digiString = n.toString().split("")
+    console.log(digiString)
     const digiInteger = digiString.map(element => parseInt(element, 10))
     let digiRecursive = digiInteger.reduce((sum, element) => sum +  element, 0)
     if (digiRecursive > 9) {
